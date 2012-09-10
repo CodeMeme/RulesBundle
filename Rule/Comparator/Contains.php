53b44493134002cec3b49ad1dac06fb4043a6f76
Christopher Hall <mythmakr@live.com>
2012-09-10 11:47:38 -0500

    Update Contains comparator to support arrays, ArrayCollections and other iterables
    Refs #6

--- Rule/Comparator/Contains.php
+++ Rule/Comparator/Contains.php
@@ -5,9 +5,23 @@ namespace CodeMeme\RulesBundle\Rule\Comparator;
 class Contains extends AbstractComparator
 {
 
-    public function compare($actual)
-    {
-        return stripos($actual, $this->expected) !== false;
-    }
+	public function compare($actual)
+	{
+		if (is_array($actual)) {
+			return in_array($this->expected, array_map(function($array) {
+				return (string) $array;
+			}, $actual));
+		}
+
+		if ($actual instanceof \IteratorAggregate) {
+			$data = iterator_to_array($actual);
+
+			return array_map(function($array) {
+				return (string) $array;
+			}, $data);
+		}
+
+		return stripos($actual, $this->expected) !== false;
+	}
 
 }
