31f6532684233a402417011d73a25811363195cb
Christopher Hall <mythmakr@live.com>
2012-09-12 11:03:42 -0500

    Updated Contains comparator to remove iterator_to_array and simply cast iterables to array

--- Rule/Comparator/Contains.php
+++ Rule/Comparator/Contains.php
@@ -5,23 +5,13 @@ namespace CodeMeme\RulesBundle\Rule\Comparator;
 class Contains extends AbstractComparator
 {
 
-	public function compare($actual)
-	{
-		if (is_array($actual)) {
-			return in_array($this->expected, array_map(function($array) {
-				return (string) $array;
-			}, $actual));
-		}
+    public function compare($actual)
+    {
+        if (is_array($actual) || $actual instanceof \IteratorAggregate) {
+            return in_array($this->expected, (array) $actual);
+        }
 
-		if ($actual instanceof \IteratorAggregate) {
-			$data = iterator_to_array($actual);
-
-			return array_map(function($array) {
-				return (string) $array;
-			}, $data);
-		}
-
-		return stripos($actual, $this->expected) !== false;
-	}
+        return stripos($actual, $this->expected) !== false;
+    }
 
 }
