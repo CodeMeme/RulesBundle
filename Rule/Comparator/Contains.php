cf32dce1a6a3e9987ea88fc6f1610c52310d4204
Christopher Hall <mythmakr@live.com>
2012-09-13 19:08:16 -0500

    Iteratively checking in_array

--- Rule/Comparator/Contains.php
+++ Rule/Comparator/Contains.php
@@ -7,8 +7,13 @@ class Contains extends AbstractComparator
 
     public function compare($actual)
     {
-        if (is_array($actual) || $actual instanceof \IteratorAggregate) {
-            return in_array($this->expected, (array) $actual);
+        if(is_array($actual) || $actual instanceof \IteratorAggregate) {
+            foreach($actual as $value) {
+                if(in_array($this->expected, (array) $value)) {
+                    return true;
+                }
+            }
+            return false;
         }
 
         return stripos($actual, $this->expected) !== false;
